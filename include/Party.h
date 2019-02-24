89771004e63595d60cf1178e87366e530ca79884
X-141 <X_141@protonmail.com>
2019-02-24 13:35:46 -0500

    quick update to Party.h with comments

--- include/Party.h
+++ include/Party.h
@@ -5,11 +5,23 @@
 #include "PlayerEntity.h"
 
 
+/*
+	Party.h:
+		Establishes a party of entities to simulate a combat party for
+		the program. Collectively these entitiy represent the status of the
+		party. If atleast one entity is alive, the party KO_Status is false.
+		Otherwise the party KO_Status is true representing a dead party.
+
+		NOTE: The only way for the party to be reorganized is to modify
+		the way the party is loaded into each party object at program startup
+*/
+
 class Party {
 protected:
 	std::vector<Entity *> m_PartyRoster;
 	int m_PartyID;
-	bool m_partyKO;
+	bool m_partyKO; // false == party is still alive
+					// true == party is dead
 public:
 	Party() {}
 	~Party();
@@ -20,10 +32,11 @@ public:
 	bool& getPartyKOStatus() { return m_partyKO; }
 	bool beginTurn(Party *opposing_party);
 	void addEntity(Entity *entity);
-	void checkPartyStatus();
+	void checkPartyStatus(); // updates the temporary states of each entity.
+							 // example if an entity has a defense boost and
+							 // it needs to be reverted back to original value.
 	void updatePartyState();
 	void printParty();
 };
 
-// bool operator<(const Entity &entity_1, const Entity &entity_2);
 
